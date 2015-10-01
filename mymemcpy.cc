3ab86811f8967a53fab9d586315dad354633d8c9
Oscar <orm@mit.edu>
2015-09-30 22:06:36 -0400

    timing

--- mymemcpy.cc
+++ mymemcpy.cc
@@ -2,6 +2,12 @@
 #include <string>
 #include <cstdlib>
 #include <cstdint>
+#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+using clk = high_resolution_clock;
+
 
 int main() {
 	uint64_t byteslen = 1 << 25; // 32MB
@@ -15,8 +21,14 @@ int main() {
 		reps = std::strtol(r, NULL, 10);
 	}
 
+
 	uint64_t * bytes = new uint64_t[byteslen/8];
+	auto startt = clk::now();
 	for  (uint64_t i = 0; i < reps; ++i) {
 		memset(bytes, i, byteslen);
 	}
+	auto endt = clk::now();
+
+	auto dur = duration_cast<milliseconds>(endt - startt).count();
+	std::cout << "{byteslen:" << byteslen << ", reps:" << reps << ", duration_millis:" << dur << "}" << std::endl;
 }

94299787e7c05b71aebbda8a4bce8f26c88eb3b3
Oscar <orm@mit.edu>
2015-09-30 21:39:02 -0400

    change

--- mymemcpy.cc
+++ mymemcpy.cc
@@ -1,21 +1,16 @@
 #include <cstring>
 #include <string>
-
+#include <stdlib.h>
 
 int main() {
   char * byteslench = getenv("BYTES");
-  uint64_t byteslen = std::strol(byteslench, NULL, 10);
-  
+  uint64_t byteslen = std::strtol(byteslench, NULL, 10);
   uint64_t * bytes = new uint64_t[byteslen/8];
 
-  char * repsch = getevn("REPS");
-  
-
-  uint64_t reps = std::strtol(repshch, NULL, 10);
+  char * repsch = getenv("REPS");
+  uint64_t reps = std::strtol(repsch, NULL, 10);
 
   for  (uint64_t i = 0; i < reps; ++i){
     memset(bytes, i, byteslen);
   }
-
-
 }

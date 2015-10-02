449d689efbf12f11b8b4df40d49d27b9528f5abd
Oscar <orm@mit.edu>
2015-10-01 22:43:51 -0400

    utils

--- common.h
+++ common.h
@@ -28,4 +28,28 @@ inline int duration_millis(auto before, auto after){
 }
 
 
+inline void output_param_names(const po::variables_map &vm){
+	for (const auto &p: vm) {
+		cout << p.first << ",";
+	}
+}
+
+inline void output_param_values(const po::variables_map &vm){
+	for (const auto &p: vm) {
+		try {
+			cout << p.second.as<string>() << ",";
+		} catch (exception &e) {
+			try {
+				cout << p.second.as<int>() << ",";
+			} catch (exception &e){
+				try {
+					cout << p.second.as<bool>() << ",";
+				} catch (exception &e) {
+					throw e;
+				}
+			}
+		}
+	}
+}
+
 #endif /* COMMON_H_ */

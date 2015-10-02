e6e1f3cfdd68ddfd61330b5fb0b2c30a06a5b580
Oscar <orm@mit.edu>
2015-10-02 00:41:26 -0400

    bugfix. plus enum

--- mymemcpy.cc
+++ mymemcpy.cc
@@ -1,16 +1,31 @@
 #include "common.h"
 #include <vector>
 #include <functional>
+#include <unordered_map>
+
+enum struct Task {
+	MEMAGG,
+	LIBCMEMSET,
+	NAIVEMEMSET
+};
 
 struct params {
 	int sizemb;
 	int reps;
 	int threads;
-	string task;
+	Task task;
 	po::variables_map * vm;
 
 	size_t getSizeB() const {
 		return ((size_t)sizemb) * (1 << 20);
+	}
+
+	void outputParamNames() const {
+		output_param_names(*vm);
+	}
+
+	void outputParamValues() const {
+		output_param_values(*vm);
 	}
 };
 
@@ -20,18 +35,17 @@ void bench_memset(const params &p) {
 
 	for (auto i = 0; i < p.threads; ++i) {
 		arrs.push_back(new uint64_t[p.getSizeB()/8]);
-		workers.emplace_back(memset, arrs[i], 10, p.getSizeB());
 	}
 
 	auto manual_var = [&](int thnum){
 		for  (int i = 0; i < p.reps; ++i) {
-			for (size_t pos = 0; pos < p.getSizeB()/8; ++pos){
+			for (size_t pos = 0; pos < p.getSizeB()/8; ++pos) {
 				arrs[thnum][pos] = i;
 			}
 		}
 	};
 
-	auto libc_memset= [&](int thnum){
+	auto libc_memset= [&](int thnum) {
 		for  (int i = 0; i < p.reps; ++i) {
 			memset(arrs[thnum], i, p.getSizeB());
 		}
@@ -39,7 +53,7 @@ void bench_memset(const params &p) {
 
 	function<void(int)> variant = libc_memset;
 
-	if (p.task == "mymemset"){
+	if (p.task == Task::NAIVEMEMSET) {
 		variant = manual_var;
 	}
 
@@ -54,9 +68,9 @@ void bench_memset(const params &p) {
 
 	auto endt = clk::now();
 
-	output_param_names(*p.vm);
+	p.outputParamNames();
 	cout << "duration_millis" << ", fork_millis" << endl;
-	output_param_values(*p.vm);
+	p.outputParamValues();
 	cout << duration_millis(startt, endt) << ", " <<  duration_millis(startt, forkt) << endl;
 }
 
@@ -98,11 +112,13 @@ void bench_agg(const params &p){
 	}
 	auto endt = clk::now();
 
-	output_param_names(*p.vm);
+	p.outputParamNames();
 	cout << "duration_millis" << ",fork_millis" << endl;
-	output_param_values(*p.vm);
+	p.outputParamValues();
 	cout << duration_millis(startt, endt) << "," <<  duration_millis(startt, forkt) << endl;
 }
+
+
 
 int main(int ac, char** av) {
 	po::options_description desc("Allowed options");
@@ -111,11 +127,17 @@ int main(int ac, char** av) {
 	    ("sizemb", po::value<int>()->default_value(32), "size in mb")
 	    ("reps", po::value<int>()->default_value(1), "number of repetitions")
 		("threads", po::value<int>()->default_value(1), "number of threads")
-		("task", po::value<string>()->default_value("memread"), "mymemset,libcmemset,memread");
+		("task", po::value<string>()->default_value("memagg"), "naivememset,libcmemset,memagg");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(ac, av, desc), vm);
 	po::notify(vm);
+
+	unordered_map<string, Task> dict {
+		{"naivememset", Task::NAIVEMEMSET},
+		{"libcmemset", Task::LIBCMEMSET},
+		{"memagg", Task::MEMAGG}
+	};
 
 	if (vm.count("help")) {
 	    cout << desc << "\n";
@@ -126,12 +148,12 @@ int main(int ac, char** av) {
 	p.sizemb = vm["sizemb"].as<int>();
 	p.reps = vm["reps"].as<int>();
 	p.threads = vm["threads"].as<int>();
-	p.task = vm["task"].as<string>();
+	p.task = dict.at(vm["task"].as<string>());
 	p.vm = &vm;
 
-	if (p.task == "mymemset" || p.task == "libcmemset") {
+	if (p.task == Task::LIBCMEMSET || p.task == Task::NAIVEMEMSET) {
 		bench_memset(p);
-	} else if (p.task == "memread") {
+	} else if (p.task == Task::MEMAGG) {
 		bench_agg(p);
 	}
 }

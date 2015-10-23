ec62e045d6836f1542e8e64fc4dce3e49fd58628
Oscar <orm@mit.edu>
2015-10-23 15:17:01 -0400

    test gather while keeping locality the same

--- tpch.cc
+++ tpch.cc
@@ -278,11 +278,12 @@ void tpch_q1_columnar_double_masked_avx256(const LineitemColumnar *l, q1result o
 }
 
 //_mm256_i32gather_epi32((const int*)$prev, $offsets, 4);
-#define gather_incr($prev, $offsets, $mask, $delta, $val_out)  \
+#define gather_incr($prev, $offsets, $mask, $delta, $val_out, $i)  \
 					do {\
-						auto currval = _mm256_load_si256($prev);\
-						auto masked_delta = _mm256_and_si256($delta, $mask);\
-						$val_out = _mm256_add_epi32(currval, masked_delta);\
+						auto currval = _mm256_load_si256(($prev) + ($i)*8);\
+						auto gather_plcholder = _mm256_xor_si256(currval, _ones); \
+						auto masked_delta = _mm256_and_si256(($delta), ($mask));\
+						($val_out) = _mm256_add_epi32(gather_plcholder, masked_delta);\
 					 } while (0)
 
 void tpch_q1_columnar_cond_avx256(const LineitemColumnar *l, q1result out, int cutoff)
@@ -323,19 +324,19 @@ void tpch_q1_columnar_cond_avx256(const LineitemColumnar *l, q1result out, int c
 			auto offsets = _mm256_add_epi32(tmp3, posv);
 
 			__m256i new_count {};
-			gather_incr(count, offsets, mask, _ones, new_count);
+			gather_incr(count, offsets, mask, _ones, new_count, (i>>3));
 
 			__m256i new_sum_qnt {};
-			gather_incr(sum_qt, offsets, mask, quantityv, new_sum_qnt);
+			gather_incr(sum_qt, offsets, mask, quantityv, new_sum_qnt, (i>>3));
 
 			__m256i new_sum_base_price {};
-			gather_incr(sum_base_price, offsets, mask, pricev, new_sum_base_price);
+			gather_incr(sum_base_price, offsets, mask, pricev, new_sum_base_price, (i>>3));
 
 			__m256i new_sum_disc_price {};
-			gather_incr(sum_disc_price, offsets, mask, discounted_pricesX100, new_sum_disc_price);
+			gather_incr(sum_disc_price, offsets, mask, discounted_pricesX100, new_sum_disc_price, (i>>3));
 
 			__m256i new_sum_charge {};
-			gather_incr(sum_charge, offsets, mask, taxed_priceX10k, new_sum_charge);
+			gather_incr(sum_charge, offsets, mask, taxed_priceX10k, new_sum_charge, (i>>3));
 
 			// manual scatter.
 			for (size_t elt = 0; elt < k_vecsize; elt+=1) {
